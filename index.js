1c237af30537ed34b37b751759006ba5ff51af67
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-08-16 07:28:02 -0400

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+let toys =["Logo", "Truck",  "Dinosaur", "Doll House", "Babie Dolls",  ]
+let price = [15, 10, 8, 20, 12]
+
+function targetSubmitButton(){
+    //1-first read the choice by the user bu document.
+      //then we need to store some where in variable
+    var storevalue = document.getElementById('targetInput').value
+//check if value = 1
+
+}
