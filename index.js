07ef661a071b6daf939113efea5044778dca77f2
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-08-17 10:58:05 -0400

    add

--- index.js
+++ index.js
@@ -6,5 +6,49 @@ function targetSubmitButton(){
       //then we need to store some where in variable
     var storevalue = document.getElementById('targetInput').value
 //check if value = 1
+if (storevalue=="1"){
+  addToys()
+}
+if (storevalue=="2"){
+  removeToys()
+}
+}
+function addToys(){
+  var toyName = prompt('Enter toy name')
+  var PR = parseFloat(prompt('Enter Price'))
+  toys.push(toyName)
+  price.push(PR)
+  alert('Added Sucessfully!')
+  printToys()
 
 }
+
+function removeToys(){
+    var toyName = prompt('Enter toy name')
+    var position = -1
+     //-1  means you are set initial value -1 coz we not found it
+  //if we find item we store on the position
+     for(i=0;i<toys.length;i++){
+      //if i find that toy if not -1 means not find the toy
+if (toys[i]== toyName){
+position = i
+break
+}
+  }
+  // if i donot find the toy the position remain -1 so i can not delete
+  if (position != -1){
+    toys.splice([position,1]) 
+    price.splice([position,1])
+    
+  }
+  printToys()
+}
+
+function printToys(){
+  var storeNamePrice = ""
+  for(i=0;i<toys.length;i++){
+    storeNamePrice += toys[i]+ " " + price[i]+ "<br>"
+  }
+  document.getElementById('showLisOfToys').innerHTML = storeNamePrice
+}
+  
